45c1aa2d4f0d077a4e48d04277378a74f9503bad
Mahmoud Sayed <mahmoudsayed1332002@gmail.com>
2023-03-18 14:29:18 -0700

    first commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - Entery point
+ *
+ * Description: A C program that prints txt using pusts
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	puts("/"Programming is like building a multilingual puzzle");
+	return 0;
+}
