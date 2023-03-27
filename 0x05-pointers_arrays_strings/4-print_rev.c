ab341d8657029b71a07f850a65b61c8cf8e701cb
Mahmoud Sayed <mahmoudsayed1332002@gmail.com>
2023-03-27 16:24:19 -0700

    solve tasks

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,9 +7,13 @@
 
 void print_rev(char *s)
 {
-	int i, r = _strlen(s);
+	int i, n;
 
-	for (i = r - 1; i >= 0; i--)
+	n = 0;
+	while (s[n] != '\0')
+		n++;
+
+	for (i = n - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints n element of an array of integers
+ * Owned by Bwave
+ * @a: int to check
+ * @n: int to check
+ * Return: 0 is success
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (x != n - 1)
+			printf("%d, ", a[x]);
+		else
+			printf("%d", a[x]);
+	}
+	printf("\n");
+}
