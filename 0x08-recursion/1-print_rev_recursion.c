eed4b03ff983d417ea3118892bfb84e505f3d036
Mahmoud Sayed <mahmoudsayed1332002@gmail.com>
2023-04-06 07:10:03 -0700

    update betty style

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * void _print_rev_recursion - Write a function that prints a string in reverse
+ * _print_rev_recursion - Write a function that prints a string in reverse
  * @s:string
  * Return: 0
  */
