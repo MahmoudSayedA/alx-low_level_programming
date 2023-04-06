395a9324b66c2f7f3e4c513069970fcd93ee8d64
Mahmoud Sayed <mahmoudsayed1332002@gmail.com>
2023-04-06 07:08:31 -0700

    add print rev

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * void _print_rev_recursion - Write a function that prints a string in reverse
+ * @s:string
+ * Return: 0
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+	return (0);
+}
+
